a98df290218f294106c3fddfb466b6c2f682fe02
pcb3 <holypallyxiii@outlook.com>
2024-01-21 12:04:26 +1300

    fix snake bug when reacting to gravity

--- src/scripts/snake.gd
+++ src/scripts/snake.gd
@@ -7,18 +7,24 @@ extends Actor
 @onready var wall_check_right = $WallCheckRight
 @onready var wall_check_left = $WallCheckLeft
 
-var direction = Vector2.RIGHT
+var direction = 1
+var grounded = false
 
 func _physics_process(delta: float) -> void:
 	
-	velocity.y += gravity * delta	
+	velocity.y += gravity * delta
+	
 	var found_ledge = not edge_check.is_colliding()
 	var found_wall = wall_check_right.is_colliding() or wall_check_left.is_colliding()
 	
-	if found_ledge or found_wall:
+	if is_on_floor():
+		grounded = true
+	else: false
+	
+	if grounded and found_ledge or found_wall:
 		direction *= -1
-
-	velocity = snake_data.speed * direction
+	
+	velocity.x = snake_data.speed * direction
 	
 	move_and_slide()
 	
